55f307d28ca3ee33f08bb22baf7bc065f4e7684d
nicholasVilela <vilela.freelance@gmail.com>
2021-07-23 11:44:56 -0400

    Added move_speed to camera movement system so when you scroll, it isn't too fast or too slow

--- src/systems/camera_movement.rs
+++ src/systems/camera_movement.rs
@@ -11,7 +11,10 @@ use amethyst::{
     renderer::{
         Camera,
     },
-    core::transform::Transform,
+    core::{
+        transform::Transform,
+        timing::Time,
+    },
     input::{
         InputHandler,
         StringBindings,
@@ -19,26 +22,40 @@ use amethyst::{
 };
 
 
-#[derive(SystemDesc, Default)]
-pub struct CameraMovementSystem;
+#[derive(SystemDesc)]
+pub struct CameraMovementSystem {
+    pub move_speed: f32,
+}
+
+impl Default for CameraMovementSystem {
+    fn default() -> Self {
+        return CameraMovementSystem {
+            move_speed: 30.0
+        };
+    }
+}
 
 impl <'s> System<'s> for CameraMovementSystem {
     type SystemData = (
         WriteStorage<'s, Transform>,
         ReadStorage<'s, Camera>,
         Read<'s, InputHandler<StringBindings>>,
+        Read<'s, Time>,
     );
 
-    fn run(&mut self, (mut transforms, cameras, input): Self::SystemData) {
-        for (transform, camera) in (&mut transforms, &cameras).join() {
+    fn run(&mut self, (mut transforms, cameras, input, time): Self::SystemData) {
+        for (transform, _) in (&mut transforms, &cameras).join() {
             let middle_down = input.action_is_down("mouse_middle").unwrap_or(false);
 
             if middle_down {
                 let d_x = input.axis_value("mouse_x").unwrap_or(0.0);
                 let d_y = input.axis_value("mouse_y").unwrap_or(0.0);
 
-                transform.move_right(d_x);
-                transform.move_up(-d_y);
+                let x = d_x * self.move_speed * time.delta_seconds();
+                let y = -d_y * self.move_speed * time.delta_seconds();
+
+                transform.move_right(x);
+                transform.move_up(y);
             }
         }
     }
